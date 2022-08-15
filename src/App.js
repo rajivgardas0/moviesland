afacfaa23bd229dc0ca51e21b365386ea4b52ddd
rajiv gardas <gardasrajiv@gmail.com>
2022-08-15 17:40:29 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard.jsx";
 
-const API_URL = "http://www.omdbapi.com?apikey=b9d561d8";
+const API_URL = "https://www.omdbapi.com?apikey=b9d561d8";
 
 const App = () => {
 
